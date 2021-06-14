9444d7e2bf4b1a407b61d6ccc2d0e7b7beac0aeb
alexandermed <alexandermed@mail.ru>
2021-06-14 17:10:28 +0300

    tasks 1-2

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Student:
+    """Класс Студент"""
+    def __init__(self, name, surname, gender):
+        """Инициализация класса"""
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def __str__(self):
+        """Вывод информации класса"""
+
+    def rate_hw(self, lecturer, course, grade):
+        """Выставление оценок лектору"""
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached \
+                and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Mentor:
+    """Класс преподавателей"""
+    def __init__(self, name, surname):
+        """Инициализация класса"""
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Lecturer(Mentor):
+    """Класс лекторов. Наследуется из класса преподавателей."""
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def __init__(self, name, surname):
+        """Инициализация класса"""
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
+
+    def __str__(self):
+        """Вывод информации класса"""
+
+class Reviewer(Mentor):
+    """Класс проверяющих. Наследуется из класса преподавателей."""
+
+    def rate_hw(self, student, course, grade):
+        """Выставление оценок студенту"""
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        """Вывод информации класса"""
+
+        return 'Имя: ' + self.name + '\n' + 'Фамилия: ' + self.surname
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Reviewer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+lect = Lecturer('Some', 'Lector')
+lect.courses_attached += ['Python']
+
+best_student.rate_hw(lect, 'Python', 10)
+
+print(best_student.grades)
+print(lect.grades)
+print(cool_mentor)
